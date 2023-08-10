16752a7a4975b2ba7bb633e83c4d418c45e062e8
abdallahaac <aceydesignz@gmail.com>
2023-08-10 07:00:49 -0400

    added placeholder images

---
+++ components/Home.vue
@@ -0,0 +1,67 @@
+<template>
+	<div class="flex w-screen h-screen">
+		<div class="relative flex w-1/2 h-full overflow-hidden">
+			<img
+				src="https://haar.qodeinteractive.com/wp-content/uploads/2018/02/home6-img1.jpg"
+				alt=""
+				srcset=""
+				class="absolute inset-0 w-full h-full object-cover z-0"
+			/>
+		</div>
+		<div class="flex w-full h-full">
+			<div class="grid h-full grid-cols-2">
+				<div
+					class="flex bg-red-500 items-center justify-center text-align font-light tracking-widest text-2xl h-full"
+				>
+					<img
+						src="https://haar.qodeinteractive.com/wp-content/uploads/2018/02/home6-img2-550x550.jpg"
+						alt=""
+						srcset=""
+						class="inset-0 object-cover h-full z-0"
+					/>
+				</div>
+				<div
+					class="flex bg-red-500 items-center justify-center text-align font-light tracking-widest text-2xl h-full"
+				>
+					<img
+						src="https://haar.qodeinteractive.com/wp-content/uploads/2018/02/home6-img3-550x550.jpg"
+						alt=""
+						srcset=""
+						class="inset-0 object-cover h-full z-0"
+					/>
+				</div>
+				<div
+					class="flex bg-red-500 items-center justify-center text-align font-light tracking-widest text-2xl h-full"
+				>
+					<img
+						src="https://haar.qodeinteractive.com/wp-content/uploads/2018/02/home6-img4-550x550.jpg"
+						alt=""
+						srcset=""
+						class="inset-0 object-cover h-full z-0"
+					/>
+				</div>
+				<div
+					class="flex bg-red-500 items-center justify-center text-align font-light tracking-widest text-2xl h-full"
+				>
+					<img
+						src="https://haar.qodeinteractive.com/wp-content/uploads/2018/02/home6-img5-550x550.jpg"
+						alt=""
+						srcset=""
+						class="inset-0 object-cover h-full z-0"
+					/>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.h-full {
+	height: 100%;
+	width: 100%;
+}
+</style>
--- layouts/MainLayout.vue
+++ layouts/MainLayout.vue
@@ -1,18 +1,7 @@
 <template>
-	<Header />
-
-	<div class="flex w-full w-screen">
-		<div class="flex flex-col w-full">
-			<div class="h-[588px] w-[200%] bg-red-300"></div>
-			<div class="h-full w-full bg-red-300"></div>
-		</div>
-
-		<div class="flex flex-wrap w-screen w-full h-[100vh]">
-			<div class="min-h-fit min-w-[420px] max-w-full bg-red-500"></div>
-			<div class="min-h-fit min-w-[420px] max-w-full bg-red-400"></div>
-			<div class="min-h-fit min-w-[420px] max-w-full bg-red-600"></div>
-			<div class="min-h-fit min-w-[420px] max-w-full bg-red-400"></div>
-		</div>
+	<div class="">
+		<Header />
+		<Home></Home>
 	</div>
 </template>
 
@@ -21,5 +10,8 @@
 <style lang="scss" scoped>
 .max-w-full {
 	width: 100%;
+}
+.mother {
+	background-color: rgb(243, 67, 73) !important;
 }
 </style>
